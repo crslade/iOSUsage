90ca4258c8747a1d1f53d1c0490d36ae8b1e671b
Christopher Slade <chris@slades.dev>
2023-10-12 12:21:25 -1000

    Initial Commit

---
+++ iOSUsage/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOSUsage
+//
+//  Created by Christopher Slade on 10/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOSUsage/iOSUsageApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSUsageApp.swift
+//  iOSUsage
+//
+//  Created by Christopher Slade on 10/12/23.
+//
+
+import SwiftUI
+
+@main
+struct iOSUsageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
