2d6ebd3000ad092e1621e4fdf3d24e5e2b9b2421
Christopher Slade <chris@slades.dev>
2023-10-12 12:28:16 -1000

    Add File Selection Button

--- iOSUsage/ContentView.swift
+++ iOSUsage/ContentView.swift
@@ -8,12 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedFile: URL? = nil
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button("Open File") {
+                let panel = NSOpenPanel()
+                panel.allowsMultipleSelection = false
+                panel.canChooseDirectories = false
+                if panel.runModal() == .OK {
+                    let url = panel.url
+                    print(url!)
+                }
+            }
         }
         .padding()
     }
